8911a6a31fbd6a90c195acac076bab9133e3e482
Scott J Dickerson <sdickers@redhat.com>
2019-08-26 11:23:44 -0400

    Add `yarn intl:report` to report on multiple i18n topics
    Run quick reports on:
    
      - Defined keys that have duplicate English text
    
      - Per locale, the percent of keys untranslated and a list of those
        keys if <20% untranslated
    
      - Translation coverage per locale
    
    The last report should line up closely with the percentages listed
    in Zanata.  If they don't match, then either new keys exist that have
    not been pushed, or some keys have duplicate text and (at this time)
    need to be manually patched into the files.
    
    Change-Id: Ifa3cf2aeb06a49ce18b8c64c52e6a6768dcc4d22
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- zanata/normalize-translations.js
+++ zanata/normalize-translations.js
@@ -2,7 +2,6 @@
 const stringify = require('json-stable-stringify')
 const fs = require('fs')
 const chalk = require('chalk')
-const table = require('table').table
 
 const filename = [
   'src/intl/translations.json',
@@ -33,82 +32,3 @@ const pretty = stringify(translatedMessages, {
 })
 
 fs.writeFileSync(filename, pretty, 'utf8')
-
-// check and report on per-locale translation coverage and untranslated keys
-function round (number, precision = 0) {
-  const factor = Math.pow(10, precision)
-  const temp = number * factor
-  const roundedTemp = Math.round(temp)
-  return roundedTemp / factor
-}
-
-function reportCoverage (englishMessages, translatedMessagesPerLocale) {
-  const messagesKeyCount = Object.keys(englishMessages).length
-
-  const report = []
-  Object.keys(translatedMessagesPerLocale).forEach(
-    locale => {
-      const localKeyCount = Object.keys(translatedMessagesPerLocale[locale]).length
-      const percent = round((localKeyCount / messagesKeyCount) * 100, 1)
-      report.push([
-        chalk`{blue ${locale}}`,
-        `${localKeyCount}/${messagesKeyCount}`,
-        chalk`{yellow ${percent}%}`
-      ])
-    }
-  )
-  console.log(table([['locale', 'keys', 'percent'], ...report], {
-    columns: {
-      0: {},
-      1: { alignment: 'right' },
-      2: { alignment: 'right' }
-    },
-    drawHorizontalLine: (index, size) => index === 0 || index === 1 || index === size
-  }))
-}
-
-function reportUntranslatedKeys (normEnglishMessages, translatedMessagesPerLocale) {
-  const untranslated = {}
-  Object.keys(translatedMessagesPerLocale).forEach(locale => {
-    untranslated[locale] = []
-    const translated = translatedMessagesPerLocale[locale]
-    Object.keys(normEnglishMessages).forEach(key => {
-      if (!translated[key]) {
-        untranslated[locale].push(key)
-      }
-    })
-  })
-
-  const untranslatedReport = []
-  Object.keys(untranslated)
-    .filter(locale => untranslated[locale].length > 0)
-    .forEach(locale => {
-      untranslatedReport.push([
-        chalk`{blue ${locale}}`,
-        untranslated[locale].sort().join('\n')
-      ])
-    })
-
-  if (untranslatedReport.length === 0) {
-    console.log(chalk`{green All keys for all locales are translated!}`)
-  } else {
-    console.log(table([ ['locale', 'keys'], ...untranslatedReport ]))
-  }
-}
-
-const englishMessages = require('../src/intl/messages')
-const normEnglishMessages = {}
-Object.keys(englishMessages).forEach(key => {
-  const { id, defaultMessage: value } = englishMessages[key]
-  normEnglishMessages[id] = value
-})
-
-const translatedMessagesPerLocale = JSON.parse(fs.readFileSync(filename, 'utf8'))
-
-console.log()
-console.log('Untranslated keys per locale:')
-reportUntranslatedKeys(normEnglishMessages, translatedMessagesPerLocale)
-
-console.log()
-console.log('Translation coverage report:')
-reportCoverage(normEnglishMessages, translatedMessagesPerLocale)
