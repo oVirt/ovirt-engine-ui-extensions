6b0f874c775437b82e5a75c03e52d99312237025
Scott J Dickerson <sdickers@redhat.com>
2019-11-11 08:57:51 -0500

    Fix StatefulModalPattern for ReactJS 16.4 changes
    In ReactJS 16.4, the `getDerivedStateFromProps` is now also called
    after a call to `setState`.  Since the `open` and `close` functions
    call `setState` to update `state.show`, and `getDerivedStateFromProps`
    always set `state.show` to the prop value (which is always true), the
    modals would never close.  Fixes:
    
      - Track the last prop value and in `getDerivedStateFromProps`, only
        update `state.show` if the property value show has changed.
    
      - Update the way props are passed along to `ModalPattern` such that
        any component extending StatefulModalPattern will not need to worry
        about those props.
    
    Change-Id: I8faa3e8f28f08b88eabd886353927d019fbf7de0
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- src/components/modals/StatefulModalPattern.js
+++ src/components/modals/StatefulModalPattern.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import ModalPattern from './ModalPattern'
-import { bindMethods, propOrState, excludeKeys } from 'patternfly-react'
-
-// TODO(vs) remove once patternfly-react PR is merged:
-//  https://github.com/patternfly/patternfly-react/pull/343
+import { excludeKeys, filterKeys } from 'patternfly-react'
 
 class StatefulModalPattern extends React.Component {
   static getDerivedStateFromProps (nextProps, prevState) {
-    return {
-      show: propOrState(nextProps, prevState, 'show')
+    if (nextProps.show !== prevState.lastPropShow) {
+      return {
+        show: nextProps.show,
+        lastPropShow: nextProps.show
+      }
     }
+
+    return null
   }
 
   constructor (props) {
     super(props)
-    this.state = { show: false }
-    bindMethods(this, ['open', 'close'])
+    this.state = {
+      show: props.show,
+      lastPropShow: props.show
+    }
+
+    this.open = this.open.bind(this)
+    this.close = this.close.bind(this)
+    this.modalPropNames = Object.keys(StatefulModalPattern.propTypes)
   }
 
   open () {
@@ -26,14 +34,12 @@ class StatefulModalPattern extends React.Component {
     this.setState({ show: false })
   }
 
-  getModalPatternProps () {
-    return this.props
-  }
-
   render () {
+    const propsForModalPattern = filterKeys(this.props, key => this.modalPropNames.includes(key))
+
     return (
       <ModalPattern
-        {...this.getModalPatternProps()}
+        {...propsForModalPattern}
         show={this.state.show}
         onClose={this.close}
       />
@@ -46,7 +52,8 @@ StatefulModalPattern.propTypes = {
 }
 
 StatefulModalPattern.defaultProps = {
-  ...excludeKeys(ModalPattern.defaultProps, ['onClose', 'show'])
+  ...excludeKeys(ModalPattern.defaultProps, ['onClose', 'show']),
+  show: false
 }
 
 export default StatefulModalPattern
