7ed332e902280ec95982f32e5394ad7861b5188c
Scott J Dickerson <sdickers@redhat.com>
2020-10-12 10:19:58 -0400

    Update Zanata tools to use the maven plugin
    Since the zanata-cli rpm is old on F30 and may not exist in more
    recent Fedora or CentOS releases, the Zanata client used has been
    updated.  Now, the Zanata maven plugin is being used.  Java 1.8 is
    still a requirement to run the plugin, just like the zanata-cli.
    
    Renamed the yarn script targets to `intl:push` and `intl:pull`
    to align with ovirt-web-ui.  README.adoc updated to reflect the
    target name changes.
    
    Change-Id: I0a197d33b50676378365dc37915da1723287b354
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

---
+++ zanata/zanata-pull.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+[[ -e zanata.xml ]] || cd zanata
+
+JAVA_HOME=$(alternatives --list | grep 'jre_1.8.0' | head -1 | cut -f 3)
+ZANATA_LOCALES=de,es,fr,it,ja,ko,pt-BR,zh-CN,cs
+
+mvn \
+    org.zanata:zanata-maven-plugin:4.6.2:pull \
+    -Dzanata.pullType="trans" \
+    -Dzanata.locales="$ZANATA_LOCALES"
