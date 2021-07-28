af8a8bac57d521e74bb504692aa98ad97aed96d5
Scott J Dickerson <sdickers@redhat.com>
2021-07-28 13:14:57 -0400

    add babel plugin fancy-console, update jest configs
    Sometimes the logging enhancements done in `src/logger.js` breaks
    some error logging.  This happen in react error logging where the
    `console.error()` string substitution characters are used in react
    code.  To step around this problem, `src/logger.js` is paired back
    to just enable turning console logging on and off and babel is used
    to add fancy output to the various logging functions.  The babel
    plugin will enhance every console.(log|info|warn|error) encountered
    in the code it processes.  Dependency code won't be touched, or will
    be properly enhanced and retain original logging functionality.
    
    The fancy logging is only applied in development (using the default
    enhanced console css features for a browser runtime) and test (using
    chalk strings for a nodejs runtime) builds.
    
    Jest configurations and transforms have been updated following the
    current state of jest in ovirt-web-ui.  All unit test now use the
    file names `*.test.js` instead of `*-test.js`.
    
    Change-Id: I970c2196585a722bb05b2e072b8652f6c7f6fe3d
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,15 @@
+const chalk = require('chalk')
+
 const env = process.env.NODE_ENV || 'development'
+const DEV = env === 'development'
 
 // babel configs
-module.exports = {
+module.exports = api => ({
   presets: [
     [
       '@babel/preset-env',
       {
-        debug: !process.env.Q && env === 'development',
+        debug: !process.env.Q && DEV,
 
         targets: {
           node: 'current',
@@ -25,4 +28,25 @@ module.exports = {
       },
     ],
   ],
-}
+
+  plugins: [
+    DEV && './babel-plugin/fancy-console',
+    api.env('test') && [
+      './babel-plugin/fancy-console',
+      {
+        loggers: [
+          {
+            object: 'console',
+            methods: ['log', 'info', 'warn', 'error'],
+            enhancements: {
+              log: [chalk.bold.white.bgHex('#21409a')(' debug ')],
+              info: [chalk.bold.white.bgHex('#01acac')(' info ')],
+              warn: [chalk.bold.white.bgHex('#f8a51b')(' warn ')],
+              error: [chalk.bold.white.bgHex('#ed403c')(' error ')],
+            },
+          },
+        ],
+      },
+    ],
+  ].filter(Boolean),
+})
--- jest.config.js
+++ jest.config.js
@@ -7,26 +7,41 @@ module.exports = {
   },
 
   moduleFileExtensions: [
-    'jsx',
     'js',
+    'jsx',
+    'ts',
+    'tsx',
+    'json',
+    'node',
   ],
   moduleDirectories: [
     'node_modules',
   ],
   moduleNameMapper: {
     '^_/(.*)$': '<rootDir>/src/$1',
-    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/jest/fileMock.js',
-    '^.+\\.(css|less)$': 'identity-obj-proxy',
   },
 
   setupFiles: [
     '<rootDir>/jest/polyfills.js',
-    '<rootDir>/jest/chalk.logger.js',
   ],
   setupFilesAfterEnv: [
     '<rootDir>/src/test-index.js',
   ],
 
+  transform: {
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': 'babel-jest',
+    '^.+\\.css$': '<rootDir>/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': '<rootDir>/jest/fileTransform.js',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '<rootDir>/**/*.test.{js,jsx,ts,tsx}',
+  ],
   testPathIgnorePatterns: [
     '<rootDir>/dist',
     '<rootDir>/exported-artifacts',
@@ -35,5 +50,4 @@ module.exports = {
     '<rootDir>/rpmbuild',
     '<rootDir>/zanata',
   ],
-  testRegex: '(/__tests__/.*|[.-]test)\\.jsx?$',
 }
