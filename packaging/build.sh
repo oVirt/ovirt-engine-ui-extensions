8909f042a9d722b01ca37f9452ba7ac22ca48e27
Scott J Dickerson <sdickers@redhat.com>
2022-02-09 12:10:33 -0500

    Refactor the build scripts
      - Combine `automation/build.sh` and `packaging/build.sh` into a
        single script
    
      - Update copr Makefile to use the new script
    
      - Update github workflows to use the new script
    
      - Update docs to reflect current state of the builds/packaging

--- packaging/build.sh
+++ packaging/build.sh
@@ -1,19 +1,44 @@
 #!/bin/bash -ex
 
-# Name and version of the package:
-tar_version="${tar_version:=0.0.0}"
-tar_file="${tar_file:=ovirt-engine-ui-extensions-${tar_version}.tar.gz}"
-rpm_version="${rpm_version:=${tar_version}}"
-rpm_snapshot="${rpm_snapshot:=}"
-rpm_dist="${rpm_dist:=$(rpm --eval '%dist')}"
+[[ "${1:-foo}" == "copr" ]] && source_build=1 || source_build=0
+[[ ${MOVE_ARTIFACTS:-1} -eq 1 ]] && use_exported_artifacts=1 || use_exported_artifacts=0
+
+# Clean the artifacts directory:
+test -d exported-artifacts && rm -rf exported-artifacts || :
+
+# Resolve the version and snapshot used for RPM build:
+version="$(jq -r '.version' package.json)"
+date="$(date --utc +%Y%m%d)"
+commit="$(git log -1 --pretty=format:%h)"
+snapshot=".${date}git${commit}"
+
+# Check if the commit is tagged (indicates a release build):
+tag="$(git tag --points-at ${commit} | grep -v jenkins || true)"
+if [ ! -z ${tag} ]; then
+  snapshot=""
+fi
+
+# Build the source tar file from git known files:
+tar_name="ovirt-engine-ui-extensions"
+tar_prefix="${tar_name}-${version}/"
+tar_file="${tar_name}-${version}${snapshot}.tar.gz"
+git archive --prefix="${tar_prefix}" --output="${tar_file}" HEAD
+
+# Create rpm build directories and place the src tar in the right place
+export top_dir="${PWD}/tmp.repos"
+test -d "${top_dir}" && rm -rf "${top_dir}" || :
+mkdir -p "${top_dir}"/{SPECS,RPMS,SRPMS,SOURCES}
+mv "${tar_file}" "${top_dir}/SOURCES"
 
 # Generate the .spec file from the template for the distribution where
 # the build process is running:
-spec_template=$(test -e "spec${rpm_dist}.in" && echo "spec${rpm_dist}.in" || echo "spec.in")
+rpm_dist="${rpm_dist:=$(rpm --eval '%dist')}"
+spec_src="${PWD}$(test -e "spec.in" && echo "" || echo "/packaging")"
+spec_template=${spec_src}/$(test -e "spec${rpm_dist}.in" && echo "spec${rpm_dist}.in" || echo "spec.in")
 spec_file="${top_dir}/SPECS/ovirt-engine-ui-extensions.spec"
 sed \
-  -e "s|@RPM_VERSION@|${rpm_version}|g" \
-  -e "s|@RPM_SNAPSHOT@|${rpm_snapshot}|g" \
+  -e "s|@RPM_VERSION@|${version}|g" \
+  -e "s|@RPM_SNAPSHOT@|${snapshot}|g" \
   -e "s|@TAR_FILE@|${tar_file}|g" \
   -e "s|@OFFLINE_BUILD@|${OFFLINE_BUILD:-1}|g" \
   < "${spec_template}" \
@@ -39,4 +64,14 @@ else
     -ba \
     --define="_topdir ${top_dir}" \
     "${spec_file}"
+fi
+
+# Copy the .tar.gz and .rpm files to the artifacts directory:
+if [[ $use_exported_artifacts -eq 1 ]] ; then
+  [[ -d exported-artifacts ]] || mkdir -p exported-artifacts
+
+  for file in $(find $top_dir -type f -regex '.*\.\(tar.gz\|rpm\)'); do
+    echo "Archiving file \"$file\"."
+    mv "$file" exported-artifacts/
+  done
 fi
